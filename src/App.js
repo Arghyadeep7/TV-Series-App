603349a6a838f63cc3d8273a4d684e6331d27309
Arghya Deep Pal <arghyadeep100@gmail.com>
2022-08-09 20:51:38 +0530

    Tv Series App Shifted to new URL

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import TvSeriesComponent from "./components/TvSeriesComponent";
-import TvSeriesDetail from "./components/TvSeriesDetail";
-import SearchQuery from "./components/SearchQuery";
-import SeasonComponent from "./components/SeasonComponent";
-import EpisodeComponent from "./components/EpisodeComponent";
-
+import { Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+
+
 
 function App() {
 
   return (
     <Container>
-      <Header/>
       <Routes>
-        <Route
-          path="/"
-          element={<Navigate replace to="/tv/trending" />}
-        />
-        <Route path="/tv/popular" element={<TvSeriesComponent type="popular"/>} />
-        <Route path="/tv/top_rated" element={<TvSeriesComponent type="top_rated"/>} />
-        <Route path="/tv/trending" element={<TvSeriesComponent type="trending"/>} />
-        <Route path="/tv/:id" element={<TvSeriesDetail />} />
-        <Route path="/tv/:id/season/:season_num" element={<SeasonComponent />} />
-        <Route path="/tv/:id/season/:season_num/episode/:episode_num" element={<EpisodeComponent />} />
-        <Route path="/search/:query" element={<SearchQuery />} />
-        <Route path="*" element={<h1>Error, this page doesnot exist !</h1>} />
+        <Route path="*" element={
+          <>
+            <br/>
+            <h1>The TV SERIES App has been shifted&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://e-hub-arghyadeep7.vercel.app/">here</a></h1>
+            <h2><a href="https://e-hub-arghyadeep7.vercel.app/">https://e-hub-arghyadeep7.vercel.app/</a></h2>
+          </>
+        } />
       </Routes>
-      <Footer />
     </Container>
   );
 }
